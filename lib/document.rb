f025e72e272cb5aecdbdadd2215334e88edd2a83
Andy Gregorowicz <andy@mitre.org>
2010-04-12 16:57:39 -0400

    impelemented delete on documents

--- lib/document.rb
+++ lib/document.rb
@@ -12,6 +12,18 @@ module HStore
             status 404
           end
         end
+        
+        delete '/records/:id/*/:doc_id' do
+          @record = Record.find(params[:id])
+          @section = @record.sections.find_by_path(params[:splat].first)
+          if @section
+            doc = @section.section_documents.find(params[:doc_id])
+            doc.destroy
+            status 204
+          else
+            status 404
+          end
+        end
       end
     end
   end
